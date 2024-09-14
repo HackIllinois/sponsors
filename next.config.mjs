b47a2a88e8732dc36626cd651fea28f4971a0675
J164 <edleyjacob@gmail.com>
2024-09-13 21:48:50 -0500

    redirect to pdf instead

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sponsors.pdf',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
